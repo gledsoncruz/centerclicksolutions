4d5a1254fbd4f12e1cd02c6e81a795b90842f50f
Gledson Cruz <gledson.cruz@gmail.com>
2016-03-11 11:00:01 -0300

    Deleta Order_Service

--- order_of_service/apps.py
+++
@@ -1,9 +0,0 @@
-# -*- coding: utf-8 -*-
-from __future__ import unicode_literals
-
-from django.apps import AppConfig
-
-
-class OrderOfServiceConfig(AppConfig):
-    name = 'order_of_service'
-    verbose_name = 'Ordem de Serviço'
--- order_of_service/views.py
+++
@@ -1,4 +0,0 @@
-from django.shortcuts import render
-
-def order_of_service_index(request):
-  return render(request, 'order_of_service/index.html', {})
